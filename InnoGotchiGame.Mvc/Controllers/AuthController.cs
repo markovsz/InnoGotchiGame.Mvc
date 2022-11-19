3923c8225780585235ddd2a3f031975a746dba1c
Stas <markovszzzz@gmail.com>
2022-11-19 18:11:46 +0300

    login endpoind was added

---
+++ InnoGotchiGame.Mvc/Controllers/AuthController.cs
@@ -0,0 +1,26 @@
+﻿using Application.Services.DataTransferObjects;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace InnoGotchiGame.Mvc.Controllers
+{
+    public class AuthController : Controller
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public AuthController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet("login")]
+        public IActionResult GetLogInPage()
+        {
+            return View("~/Views/LogIn.cshtml");
+        }
+    }
+}
