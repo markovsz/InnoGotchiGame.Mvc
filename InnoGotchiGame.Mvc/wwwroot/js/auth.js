d204cdb0317e21a7842815177130765be5f5ff8b
Stas <markovszzzz@gmail.com>
2022-11-22 13:11:10 +0300

    login page with js logic was added

---
+++ InnoGotchiGame.Mvc/wwwroot/js/auth.js
@@ -0,0 +1,17 @@
+﻿function submitLoginForm() {
+    let emailValue = document.getElementById('email-input').value;
+    let passwordValue = document.getElementById('password-input').value;
+    fetch('https://localhost:44336/api/Auth/signIn', {
+        method: 'POST',
+        mode: 'cors',
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email: emailValue, password: passwordValue })
+    })
+    .then(response => response.json())
+    .then((data) => {
+        localStorage.setItem('jwtToken', data.jwtToken);
+    });
+}
