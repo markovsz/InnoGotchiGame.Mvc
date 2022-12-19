37a733d70375800903348f05fb458e011c009aa1
Stas <markovszzzz@gmail.com>
2022-12-20 01:53:56 +0300

    Innogotchi details page was added

--- InnoGotchiGame.Mvc/Controllers/PetsController.cs
+++ InnoGotchiGame.Mvc/Controllers/PetsController.cs
@@ -1,18 +1,72 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Services.DataTransferObjects.Reading;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Net.Http.Headers;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace InnoGotchiGame.Mvc.Controllers
 {
     public class PetsController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public PetsController(IHttpClientFactory httpClientFactory)
         {
-            return View();
+            _httpClientFactory = httpClientFactory;
         }
 
+        [HttpGet]
+        [Route("pets/pet/{petId}/details")]
+        public async Task<IActionResult> PetOverview(Guid petId)
+        {
+            var jwtToken = Request.Cookies["jwtToken"];
+            var petsRequestMessage = new HttpRequestMessage(
+            HttpMethod.Get,
+            $"https://localhost:44336/api/Pets/pet/{petId}")
+            {
+                Headers =
+                {
+                    { HeaderNames.Accept, "application/json" },
+                    { HeaderNames.Authorization, $"Bearer {jwtToken}" }
+                }
+            };
 
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var petsResponseMessage = await httpClient.SendAsync(petsRequestMessage);
+            var petsJson = await petsResponseMessage.Content.ReadAsStringAsync();
+            var pet = JsonConvert.DeserializeObject<PetReadingDto>(petsJson);
+            ViewBag.Pet = pet;
+            return View("~/Views/InnogotchiDetails.cshtml");
+        }
+
+        [HttpGet]
+        [Route("pets/list")]
+        public async Task<IActionResult> PetsOverview()
+        {
+            var jwtToken = Request.Cookies["jwtToken"];
+            var petsRequestMessage = new HttpRequestMessage(
+            HttpMethod.Get,
+            "https://localhost:44336/api/Pets/all")
+            {
+                Headers =
+                {
+                    { HeaderNames.Accept, "application/json" },
+                    { HeaderNames.Authorization, $"Bearer {jwtToken}" }
+                }
+            };
+
+
+            var httpClient = _httpClientFactory.CreateClient();
+            var petsResponseMessage = await httpClient.SendAsync(petsRequestMessage);
+            var petsJson = await petsResponseMessage.Content.ReadAsStringAsync();
+            var pets = JsonConvert.DeserializeObject<IEnumerable<PetMinReadingDto>>(petsJson);
+            ViewBag.Pets = pets;
+            return View("~/Views/AllInnogotchiList.cshtml");
+        }
     }
 }
--- InnoGotchiGame.Mvc/Models/Reading/PetReadingDto.cs
+++ InnoGotchiGame.Mvc/Models/Reading/PetReadingDto.cs
@@ -11,5 +11,21 @@ namespace Application.Services.DataTransferObjects.Reading
         public string ThirstLevel { get; set; }
         public int HappinessDaysCount { get; set; }
         public string IsAlive { get; set; }
+        public string BodyPicName { get; set; }
+        public float BodyPictureX { get; set; }
+        public float BodyPictureY { get; set; }
+        public float BodyPictureScale { get; set; }
+        public string EyesPicName { get; set; }
+        public float EyesPictureX { get; set; }
+        public float EyesPictureY { get; set; }
+        public float EyesPictureScale { get; set; }
+        public string MouthPicName { get; set; }
+        public float MouthPictureX { get; set; }
+        public float MouthPictureY { get; set; }
+        public float MouthPictureScale { get; set; }
+        public string NosePicName { get; set; }
+        public float NosePictureX { get; set; }
+        public float NosePictureY { get; set; }
+        public float NosePictureScale { get; set; }
     }
 }
